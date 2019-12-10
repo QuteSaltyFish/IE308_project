bebd254eb7301928c20beeb8ca7d2104bac6ff95
AxelTchaikovsky <lzx1999sjtu@gmail.com>
2019-12-10 22:09:50 +0800

    rgb change

--- dev/calHist.py
+++ dev/calHist.py
@@ -3,9 +3,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('data/train_origin/SK BR 959 YAN XU FANG F55Y_20160719_151147_image.jpg')
+img2 = cv2.imread('data/train_noise/SK BR 959 YAN XU FANG F55Y_20160719_151147_image.jpg')
+img_diff = np.abs(img2 - img)
 color = ('b','g','r')
 for i,col in enumerate(color):
-    histr = cv2.calcHist([img],[i],None,[256],[0,256])
+    histr = cv2.calcHist([img_diff],[i],None,[256],[0,256])
     plt.plot(histr,color = col)
     plt.xlim([0,256])
 plt.show()
+cv2.imwrite('data/test.jpg',img_diff)
