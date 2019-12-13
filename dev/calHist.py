679050967063ff5c104f1d8ebb175492b92262e1
AxelTchaikovsky <lzx1999sjtu@gmail.com>
2019-12-13 15:00:15 +0800

    test

--- dev/calHist.py
+++ dev/calHist.py
@@ -3,7 +3,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('data/train_origin/SK BR 959 YAN XU FANG F55Y_20160719_151147_image.jpg')
+img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 img2 = cv2.imread('data/train_noise/SK BR 959 YAN XU FANG F55Y_20160719_151147_image.jpg')
+img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2HSV)
+
 img_diff = np.abs(img2 - img)
 color = ('b','g','r')
 for i,col in enumerate(color):
