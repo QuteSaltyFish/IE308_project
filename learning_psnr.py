a5fedd2c8597568b1fc424744e26f03f16839965
AxelTchaikovsky <lzx1999sjtu@gmail.com>
2019-12-17 21:33:49 +0800

    change

--- learning_psnr.py
+++ learning_psnr.py
@@ -11,3 +11,11 @@ img2 = cv2.resize(img2, (256, 256), interpolation=cv2.INTER_CUBIC)
 psnr = PSNR(img2, img4)
 print ("The PSNR between the two img of the two is %f" % psnr)
 
+img = cv2.imread('C:/Users/ZXLi/Pictures/idx_11.jpg')
+print(img.shape)
+img4 = img[:,256:512,:]
+img2 = cv2.imread('D:/IE308_project/data/train_origin/SK BR 1353 XU MING LI F64Y_20161018_105257_image.jpg')
+img2 = cv2.resize(img2, (256, 256), interpolation=cv2.INTER_CUBIC)
+
+psnr = PSNR(img2, img4)
+print ("The PSNR between the two img of the two is %f" % psnr)
