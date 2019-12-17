1db9041c357720c25c7af3cd46b05071eef9658f
QuteSaltyFish <wangmingke@sjtu.edu.cn>
2019-12-17 20:41:25 +0800

    fix psnr calculation

--- model/PSNR.py
+++ model/PSNR.py
@@ -4,14 +4,15 @@ import numpy
 
 
 def PSNR(img1, img2):
-    D = numpy.array(img1 - img2, dtype=numpy.int64)
-    D[:, :] = D[:, :]**2
-    RMSE = D.sum()/img1.size
+    D = numpy.array(img1 - img2)
+    RMSE = numpy.sum(D**2)/img1.size
     psnr = 10*math.log10(float(255.**2)/RMSE)
     return psnr
 
-#if __name__ == "__main__":
-    # img1 = cv2.imread("", cv2.IMREAD_GRAYSCALE)
-    # img2 = cv2.imread("", cv2.IMREAD_GRAYSCALE)
-    # psnr = PSNR(img1, img2)
-    # print ("The PSNR between the two img of the two is %f" % psnr)
+if __name__ == "__main__":
+    
+    name = 'BR 176 YUAN XIA F39Y_20160113_133403_image.jpg'
+    oripic = cv2.imread('data/train_origin/'+name,0)
+    result = cv2.imread('data/train_noise/'+name,0)
+    psnr = PSNR(oripic, result)
+    print ("The PSNR between the two img of the two is %f" % psnr)
